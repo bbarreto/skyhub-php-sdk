e5dc1616aee2d889dc29c0a4c75324b3a37b65b8
Bruno Barreto <bruno@brunobarreto.net>
2017-05-15 12:12:42 -0300

    Innitial Commit

---
+++ src/Marketplace.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Skyhub;
+
+class Marketplace {
+	
+	protected $conf;
+
+	public function __construct() {
+		$this->conf = (object) [
+			'endpoint'=>'https://api.skyhub.com.br'
+		];
+	}
+
+	public function setAuth($email, $key) {
+		$this->conf->auth->email = $email;
+		$this->conf->auth->key = $key;
+	}
+
+	public function products($sku=null) {
+		$products = new Resources\Products($this);
+
+		if (!is_null($sku)):
+			$products->setSku($sku);
+		endif;
+
+		return $products;
+	}
+
+	public function apiCall($type='GET', $uri='/', $data=[]) {
+		$url = $this->conf->endpoint.$uri;
+		$opts = [
+		    "http" => [
+		        "method" => $type,
+		        "ignore_errors"=>true,
+		        "header" => "Content-type: application/json\r\n".
+		        			"Accept: application/json\r\n".
+						    "X-Api-Key: {$this->conf->auth->key}\r\n".
+						    "X-User-Email: {$this->conf->auth->email}"
+		    ]
+		];
+
+		if (is_array($data) && $type=='GET'):
+			$url .= '?'.http_build_query($data);
+		else:
+			$opts['http']['content'] = json_encode($data);
+		endif;
+
+		try {
+			$context = stream_context_create($opts);
+			$json = file_get_contents($url, false, $context);
+			$headers = $this->parseHeaders($http_response_header);
+
+			$json = json_decode($json, true);
+
+			if (isset($json['error'])):
+				return ['error'=>['message'=>$json['error'], 'code'=>$headers['response_code']]];
+			endif;
+
+			switch ($headers['response_code']) {
+				case 201:
+					return true;
+					break;
+				case 204:
+					return true;
+					break;
+				case 400:
+					return ['error'=>['code'=>401, 'message'=>'Formato de requisição inválido.']];
+					break;
+				case 401:
+					return ['error'=>['code'=>401, 'message'=>'E-mail ou chave de acesso inválidos.']];
+					break;
+				case 403:
+					return ['error'=>['code'=>403, 'message'=>'Autenticação inválida']];
+					break;
+				case 404:
+					return ['error'=>['code'=>404, 'message'=>'Objeto não encontrado.']];
+					break;
+			}
+
+			if ($json==null):
+				return ['error'=>['message'=>'API SkyHub não retornou resposta.', 'code'=>$headers['response_code']]];
+			endif;
+
+			return $json;
+
+		} catch (Exception $e) {
+			return $e->getMessage();	
+		}
+	}
+
+	private function parseHeaders($headers) {
+	    $head = array();
+	    foreach($headers as $k=>$v):
+	        $t = explode(':', $v, 2);
+	        if(isset($t[1])):
+	            $head[trim($t[0])] = trim($t[1]);
+	        else:
+	            $head[] = $v;
+	            if(preg_match("#HTTP/[0-9\.]+\s+([0-9]+)#",$v, $out))
+	                $head['response_code'] = intval($out[1]);
+	        endif;
+	    endforeach;
+	    return $head;
+	}
+
+}
---
+++ src/Resources/Products.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Skyhub\Resources;
+
+class Products extends \Skyhub\Marketplace {
+
+	private $sku=null,
+	        $params;
+
+	public function __construct($data) {
+		$this->conf = $data->conf;
+	}
+
+	/**
+	 * Definir SKU padrão
+	 * @param type $sku 
+	 * @return type
+	 */
+	public function setSku($sku) {
+		$this->sku = $sku;
+	}
+
+	/**
+	 * Listar todos os produtos cadastrados
+	 * @param type|int $page Número da página
+	 * @param type|int $per_page Quantidade de resultados por página
+	 * @return type|array Lista de produtos
+	 */
+	public function get($page=1, $per_page=25) {
+
+		if (isset($page) && (!is_numeric($page) || $page<1)):
+			return ['error'=>['code'=>null, 'message'=>'Parâmetro page deve ser um número inteiro maior que zero.']];
+		endif;
+
+		if (isset($per_page) && (!is_numeric($per_page) || $per_page<1)):
+			return ['error'=>['code'=>null, 'message'=>'Parâmetro per_page deve ser um número inteiro maior que zero.']];
+		endif;
+
+		$this->params = [
+			'page'=>$page,
+			'per_page'=>$per_page
+		];
+
+		return $this->apiCall('GET', '/products', $this->params);
+	}
+
+	/**
+	 * Consultar um único produto a partir do SKU
+	 * @param type $sku 
+	 * @return type|array 
+	 */
+	public function getBySku() {
+		if (is_null($this->sku))
+			return ['error'=>'Informe o SKU do produto'];
+
+		return $this->apiCall('GET', '/products/'.$this->sku);
+	}
+
+	/**
+	 * Receber próxima página de resultados
+	 * @return type
+	 */
+	public function nextPage() {
+
+		if (isset($this->params->page) && is_numeric($this->params->page)):
+			$this->params->page++;
+		else:
+			$this->params->page = 2;
+		endif;
+
+		return $this->apiCall('GET', '/products', $this->params);
+	}
+
+	/**
+	 * Inserir produto
+	 * @param type $product 
+	 * @return type
+	 */
+	public function insert($product) {
+
+		if (!is_array($product))
+			return ['error'=>'Produto deve ser um array.'];
+
+		return $this->apiCall('POST', '/products', ['product'=>$product]);
+
+	}
+
+	/**
+	 * Atualizar produto
+	 * @param type $product 
+	 * @return type
+	 */
+	public function update($product) {
+
+		if (!is_array($product))
+			return ['error'=>'Produto deve ser um array.'];
+
+		return $this->apiCall('PUT', '/products/'.urlencode($this->sku), ['product'=>$product]);
+
+	}
+
+	/**
+	 * Remover produto
+	 * @return type
+	 */
+	public function delete() {
+
+		if (is_null($this->sku))
+			return ['error'=>'Informe o SKU do produto'];
+
+		return $this->apiCall('DELETE', '/products/'.urlencode($this->sku));
+
+	}
+
+	/**
+	 * Adicionar variação a um produto
+	 * @param type $variation 
+	 * @return type
+	 */
+	public function addVariation($variation) {
+
+		if (is_null($this->sku))
+			return ['error'=>'Informe o SKU do produto para adicionar a variação'];
+
+		if (!is_array($variation))
+			return ['error'=>'Variação deve ser um array.'];
+
+		return $this->apiCall('POST', '/products/'.urlencode($this->sku).'/variations', ['variation'=>$variation]);
+		
+	}
+
+	public function urls($mktplace=null) {
+		if (is_null($mktplace))
+			return $this->apiCall('GET', '/products/urls');
+		else
+			return $this->apiCall('GET', '/products/urls/'.urlencode($mktplace));
+	}
+
+}
